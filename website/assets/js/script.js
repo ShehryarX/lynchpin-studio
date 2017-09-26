157a6ae5ccf952b3ad9dd563204917dc1600d680
Shehryar <xshehryar@gmail.com>
2017-09-26 07:29:47 -0400

    Initial commit

---
+++ website/assets/js/script.js
@@ -0,0 +1,13 @@
+/*
+  Google Analytics Configuratio file.
+*/
+
+'use strict';
+
+$(function() {
+  thesaas.config({
+    googleApiKey: 'AIzaSyDRBLFOTTh2NFM93HpUA4ZrA99yKnCAsto',
+    googleAnalyticsId: 'UA-73325209-2',
+    smoothScroll: true
+  });
+});
